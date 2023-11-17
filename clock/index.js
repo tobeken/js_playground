f6fe4317aca49314afd845372874d806b3f1de03
tobioka <tobioka@ip-10-241-3-74.ap-northeast-1.compute.internal>
2023-11-17 22:33:15 +0900

    clock

---
+++ clock/index.js
@@ -0,0 +1,19 @@
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+
+const updateClock = () => {
+    const currentDate = new Date();
+    setTimeout(updateClock,1000);
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
+    const second = currentDate.getSeconds();
+    const hourDeg = (hour / 12) * 360;
+    hourEl.style.transform = `rotate(${hourDeg}deg)`;
+    const minuteDeg = (minute / 60) * 360;
+    hourEl.style.transform = `rotate(${minuteDeg}deg)`;
+    const secondDeg = (second / 60) * 360;
+    hourEl.style.transform = `rotate(${secondDeg}deg)`;
+}
+
+updateClock();
